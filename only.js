31383ff760cf637aea3128de2b616276d55375be
jacob5555555555 <jacobeliasprinz@gmail.com>
2015-07-25 09:44:28 -0400

    encapsulated only.js inside of only object  Please enter the commit message for your changes. Lines starting

--- only.js
+++ only.js
@@ -1,80 +1,84 @@
-var content = jQuery("#content");
+var only = function(){
+	var content = jQuery("#content");
+	var callbacks = {};//XXX make callbacks get deleted when element is deleted, or not global
 
-function parseHtmlObj(obj) {
-	if (obj instanceof Object) {
-		var keys = [];
-		var attrList = [];
+	function parseHtmlObj(obj) {
+		if (obj instanceof Object) {
+			var keys = [];
+			var attrList = [];
 
-		var elements = Object.keys(obj);
-		name = elements[0];
+			var elements = Object.keys(obj);
+			name = elements[0];
 
-		for (var i = 1; i < elements.length; ++i) {
-			var el = elements[i];
-			attrList.push(el + "=" + JSON.stringify(obj[el]));
+			for (var i = 1; i < elements.length; ++i) {
+				var el = elements[i];
+				attrList.push(el + "=" + JSON.stringify(obj[el]));
+			}
+			var attr = " " + attrList.join(" ");
+
+			var value = obj[name];
+			return parseNameandValue(name, value, attr);
+		} else {
+			return JSON.stringify(obj);
 		}
-		var attr = " " + attrList.join(" ");
-
-		var value = obj[name];
-		return parseNameandValue(name, value, attr);
-	} else {
-		return JSON.stringify(obj);
 	}
-}
 
-var callbacks = {};//XXX make callbacks not global
-function parseNameandValue(name, value, attr) {
-	var valStr;
-	if (value instanceof Array) {
-		valStr = parseHtmlList(value);
-	} else if (value instanceof Function) {
-		var hash = "asdf";//XXX make actual random string
-		var dataId = 'data-only-id="' + hash + '"';
-		attr += ' ' + dataId;
-		callbacks[dataId] = value;
-		valStr = "temp";
-	}else {
-		valStr = value;
+	function parseNameandValue(name, value, attr) {
+		var valStr;
+		if (value instanceof Array) {
+			valStr = parseHtmlList(value);
+		} else if (value instanceof Function) {
+			var hash = "" + Object.keys(callbacks).length;
+			var dataId = 'data-only-id="' + hash + '"';
+			attr += ' ' + dataId;
+			callbacks[dataId] = value;
+			valStr = "";
+		}else {
+			valStr = value;
+		}
+		return "<" + name + attr + ">" + valStr + "</" + name + ">";
 	}
-	return "<" + name + attr + ">" + valStr + "</" + name + ">";
-}
 
-function getByDataId(dataId){
-	var element = document.querySelectorAll('[' + dataId + ']')
-	return element;
-}
-
-function parseHtmlList(list) {
-	var strList = [];
-	for ( var i in list) {
-		var el = list[i];
-		strList.push(parseHtmlObj(el));
+	function getByDataId(dataId){
+		var element = document.querySelectorAll('[' + dataId + ']')
+		return element;
 	}
-	return strList.join("");
-}
 
-function makeHtml(html) {
-	var html = parseHtmlList(html);
-	document.body.innerHTML = html;
-	for (var id in callbacks){
-		callbacks[id](getByDataId(id));
+	function parseHtmlList(list) {
+		var strList = [];
+		for ( var i in list) {
+			var el = list[i];
+			strList.push(parseHtmlObj(el));
+		}
+		return strList.join("");
 	}
-}
 
-function makeCss(name, css){
-	var sheet = document.createElement('style');
-	sheet.innerHTML = genCss(name, css);
-	console.log(genCss(name, css));
-	document.body.appendChild(sheet);
-}
-
-function genCss(name, css){
-	cssText = [];
-	cssText.push(name);
-	cssText.push('{');
-	for (var el in css){
-		cssText.push(el+":");
-		cssText.push(css[el]+";");
+	function genCss(name, css){
+		cssText = [];
+		cssText.push(name);
+		cssText.push('{');
+		for (var el in css){
+			cssText.push(el+":");
+			cssText.push(css[el]+";");
+		}
+		cssText.push('}');
+		return cssText.join('');
 	}
-	cssText.push('}');
-	return cssText.join('');
-}
+	
+	return {
+		makeHtml: function(html) {
+			var html = parseHtmlList(html);
+			document.body.innerHTML = html;
+			for (var id in callbacks){
+				callbacks[id](getByDataId(id));
+			}
+		},
+
+		makeCss: function(name, css){
+			var sheet = document.createElement('style');
+			sheet.innerHTML = genCss(name, css);
+			console.log(genCss(name, css));
+			document.body.appendChild(sheet);
+		}
+	}
+}();
