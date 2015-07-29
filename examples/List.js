0b178b2acda59cab3cee19a315313497221e9d40
Jacob Prinz <jprinz@umd.edu>
2015-07-29 15:37:38 -0400

    Added to List.js example

--- examples/List.js
+++ examples/List.js
@@ -1,34 +1,74 @@
 $(document).ready(function(){
 	function listHolder(){
-		var borderCss = {
+
+		var borderWidth = 10 + "px";
+
+		//CSS
+		var holderCss = {
+			display: "inline-block",
+			borderSpacing: "0px",
 			borderStyle: "solid",
-			borderWidth: "10px"
-		}
-		var holderCss = only.merge(borderCss, {
-			height: "200px",
-			width: "200px"
-			});
-		var html = only.html({
-			div: [],
-			css: holderCss 
+			borderWidth: borderWidth
+		};
+		var dividerCss = {
+			backgroundColor: 'black',
+			height: "100%",
+			width: borderWidth
+		};
+		//HTML
+		var holder = only.html({
+			tr: []
 		});
+		var html = only.html({
+			table: holder,
+			css: holderCss
+		});
+		function divider(){
+			return only.html({
+				td: "",
+				css: dividerCss
+			});
+		}
+
+		//logic
+		var isEmpty = true;
+		function addElement(el){
+			if (!isEmpty){
+				holder.appendChild(divider());
+			} else {
+				isEmpty = false;
+			}
+			var newChild = (only.html({
+				td: [
+				     el,
+				     ]
+			}));
+			holder.appendChild(newChild);
+		}
+
 		return {
 			html: html,
-			addElement: function(el){
-				var newChild = (only.html({
-					div: [
-						el,
-					],
-					css: borderCss
-				}));
-				html.appendChild(newChild);
-			}
+			addElement: addElement
 		};
 	}
 	var lh = listHolder();
-	lh.addElement(only.html({p: 'hi'}));
+	lh.addElement(only.html({p: 'hi, yo@'}));
+	lh.addElement(only.html({
+		div: [
+		      {p: "1"},
+		      {p: "2"},
+		      {p: "3"}
+		      ]
+	}))
 	lh.addElement(only.html({p: 'bye'}));
+	
+	var lh2 = listHolder();
+	for (var i = 0; i < 10; ++i){
+		lh2.addElement(only.html({p: 'asdfl;kjqweproiu'}));
+	}
+	
 	only.setHtml([
-	              lh.html
+	              lh.html,
+	              lh2.html
 	              ]);
 });
